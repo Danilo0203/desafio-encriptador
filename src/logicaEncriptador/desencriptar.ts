1c90ed371b1c65ed07fdcbb91e194c55243c2a21
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-02-16 17:48:04 -0600

    arreglar funcion desencriptar

--- src/logicaEncriptador/desencriptar.ts
+++ src/logicaEncriptador/desencriptar.ts
@@ -1,9 +1,22 @@
 export const desencriptar = (texto: string): string => {
-  let textoDesencriptado = texto
-    .replace(/enter/gi, "e")
-    .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
-    .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
+  let textoDesencriptado = texto.replace(
+    /(enter|imes|ober|ufat|ai)/gi,
+    (match, palabraEncriptada) => {
+      switch (palabraEncriptada.toLowerCase()) {
+        case "enter":
+          return "e";
+        case "imes":
+          return "i";
+        case "ober":
+          return "o";
+        case "ufat":
+          return "u";
+        case "ai":
+          return "a";
+        default:
+          return match; // En caso de que ninguna palabra coincida, retornar la palabra encriptada original
+      }
+    },
+  );
   return textoDesencriptado;
 };
